c19feafb27f3275f1d8566907f206e6d7688d0ce
NeoTyger <neojl333@gmail.com>
2023-03-30 01:00:35 +0200

    Commit

--- Assets/Scripts/BotController.cs
+++ Assets/Scripts/BotController.cs
@@ -8,10 +8,7 @@ public class BotController : MonoBehaviour
 {
 
     private Animator animator;
-    public float speed = 10f;
-    public float jumpForce = 5f;
-    private bool onGround = false;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,60 +18,38 @@ public class BotController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.W))
         {
-            animator.SetInteger("state",1);
-            
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        }else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
-        {
-            animator.SetInteger("state",2);
-            
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            animator.SetInteger("state",-1);
-            
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
+            animator.SetInteger("state",1); // Walk
         }
         
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.LeftShift))
         {
-            animator.SetInteger("state",-1);
-            
-            transform.Translate(Vector3.right * Time.deltaTime * speed);
+            animator.SetInteger("state",2); // Run
         }
         
-        if (Input.GetKey(KeyCode.S))
-        {
-            animator.SetInteger("state",-1);
-            
-            transform.Translate(Vector3.back * Time.deltaTime * speed);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Space) && onGround == true)
-        {
-            onGround = false;
-            
-            animator.SetInteger("state",3);
-            
-            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        else if (Input.GetKey(KeyCode.Space))
+        { 
+            animator.SetInteger("state",3); // Jump
         }
 
+        else if(Input.GetKey(KeyCode.D))
+        {
+            animator.SetInteger("state",-1); // Turn left
+        }
+        
+        else if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetInteger("state",-2); // Turn right
+        }
+        
+        else if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetInteger("state",-3); // Turn
+        }
         else
         {
-            animator.SetInteger("state",0);
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            onGround = true;
+            animator.SetInteger("state", 0); // Idle
         }
     }
 }
